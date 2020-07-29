bde8244b657f1a4cb0f5153cdfd69be41f00538c
Bizzy211 <Bizzy211@gmail.com>
2020-07-28 21:58:36 -0400

    Ready

--- cisco-automation-tutorial.py
+++ cisco-automation-tutorial.py
@@ -23,3 +23,14 @@ interface_filter = '''
 # Execute the get-config RPC
 result = m.get_config('running', interface_filter)
 print(result)
+
+# Render our Jinja template
+interface_template = Template(open('interface.xml').read())
+interface_rendered = interface_template.render(
+  INTERFACE_INDEX='2', 
+  IP_ADDRESS='10.0.0.1', 
+  SUBNET_MASK='255.255.255.252'
+)
+# Execute the edit-config RPC
+result = m.edit_config(target='running', config=interface_rendered)
+print(result)
